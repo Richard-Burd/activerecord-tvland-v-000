46e93a95e4287e3f18cef1aeb638dad279e36b6f
Richard Burd <richardburdx@yahoo.com>
2017-10-23 23:16:58 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -2,7 +2,7 @@ class CreateActors < ActiveRecord::Migration[4.2]
   def change
     create_table :actors do |table|
       table.string :last_name
-      table.string :firt_name
+      table.string :first_name
     end
   end
 end
